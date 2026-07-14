50876c8146dbcd3f6db0535d8975bb9d487bcbdd
gudenamrutha <gudenamrutha@gmail.com>
2026-07-14 18:59:25 +0200

    Wire up DB for ChatDbContext and IchatClient

--- DotnetAiChat.Web/Program.cs
+++ DotnetAiChat.Web/Program.cs
@@ -1,10 +1,33 @@
+using DotnetAiChat.Core.Data;
 using DotnetAiChat.Web.Components;
+using Microsoft.Extensions.AI;
+using OpenAI;
+using System.ClientModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<ChatDbContext>();
+builder.Services.AddSingleton<IChatClient>(sp =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+    var credential = new ApiKeyCredential(config["GitHubModels:Token"]
+        ?? throw new InvalidOperationException("GitHub token not found in user secrets"));
+
+    var options = new OpenAIClientOptions()
+    {
+        Endpoint = new Uri("https://models.github.ai/inference")
+    };
+
+    return new OpenAIClient(credential, options)
+        .GetChatClient("openai/gpt-4o-mini")
+        .AsIChatClient();
+}
+
+);
 
 var app = builder.Build();
 
